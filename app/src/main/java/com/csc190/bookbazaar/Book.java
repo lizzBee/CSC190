abd742ba6141dea1acad90c173600c52ca613377
Charlie <cpederson731@gmail.com>
2020-10-21 21:27:34 -0400

    Added Cian's xml files

--- app/src/main/java/com/csc190/bookbazaar/Book.java
+++ app/src/main/java/com/csc190/bookbazaar/Book.java
@@ -1,6 +1,24 @@
 package com.csc190.bookbazaar;
 
 public class Book {
+    String author, title, hofstraID, subject, bookURL;
+    int ISBN, condition, price;
+    private boolean isStarred;
+
+    public Book(String writer, String bookTitle, String hofID, String sub, String URL, int ISBNnum, int cond, int prix, boolean star) {
+        author = writer;
+        title = bookTitle;
+        hofstraID = hofID;
+        subject = sub;
+        bookURL = URL;
+        ISBN = ISBNnum;
+        condition = cond;
+        price = prix;
+        isStarred = star;
+    }
+
+
+
 
 
 }
