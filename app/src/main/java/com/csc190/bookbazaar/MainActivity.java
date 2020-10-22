522f7b79f3fd2114f6f52227170f987a4d6d4285
Charlie <cpederson731@gmail.com>
2020-10-21 20:27:56 -0400

    Added user.java and registration.java, implemented all of the files to make the database work for Registration.

--- app/src/main/java/com/csc190/bookbazaar/MainActivity.java
+++ app/src/main/java/com/csc190/bookbazaar/MainActivity.java
@@ -3,11 +3,13 @@ package com.csc190.bookbazaar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static com.csc190.bookbazaar.R.*;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.registration);
     }
 }
