1676401c59d407afec5936493fcd88ea416761a3
lizzBee <ehorishny@gmail.com>
2020-11-07 07:17:14 -0500

    completed profile class & fixed nav
    Signed-off-by: lizzBee <ehorishny@gmail.com>

--- app/src/main/java/com/csc190/bookbazaar/Profile.java
+++ app/src/main/java/com/csc190/bookbazaar/Profile.java
@@ -1,42 +1,76 @@
 package com.csc190.bookbazaar;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 
 public class Profile extends Activity {
     FirebaseDatabase database;// = FirebaseDatabase.getInstance();
     DatabaseReference myRef;// = database.getReference("user");
-
+    FirebaseAuth mAuth;
+    FirebaseUser user;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
-
+        user = FirebaseAuth.getInstance().getCurrentUser();
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference("User"); // .child(user_key) to get the user logged in
-
+        mAuth = FirebaseAuth.getInstance();
         // 3 texts, 1 button
         final Button editProfile;
-        final TextView user, email, pass;
+        final TextView email;
+        email = findViewById(R.id.textView25);
+        String em = user.getEmail();
+       // String passWord = user.getPass();
+      //  String eMail = us.getEmail();
+//
+       // user.setText(userName);
+        email.setText(em);
+       // pass.setText(passWord);
+        editProfile = findViewById(R.id.button8);
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(getApplicationContext(), EditProfile.class));
+            }});
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        editProfile = (Button)findViewById(R.id.button8);
-
-        user = (TextView)findViewById(R.id.textView7);
-        email = (TextView)findViewById(R.id.textView25);
-        pass = (TextView)findViewById(R.id.textView9);
-
+        bottomNavigationView.setSelectedItemId(R.id.tab_profile);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.tab_home:
+                        startActivity(new Intent(getApplicationContext(), Home.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.tab_starred:
+                        startActivity(new Intent(getApplicationContext(), Starred.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.tab_yourlistings:
+                        startActivity(new Intent(getApplicationContext(), my_listing.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.tab_profile:
+                        return true;
+                }
+                return false;
+            }
+        });
         /*
         String user_key = "";
 
