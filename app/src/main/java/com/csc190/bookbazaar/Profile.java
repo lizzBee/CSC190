46ea7e389808bb6b1d46d0637741e5bfc2e1ce42
lizzBee <ehorishny@gmail.com>
2020-11-07 14:34:26 -0500

    username and hofid now read from fireStore
    Signed-off-by: lizzBee <ehorishny@gmail.com>

--- app/src/main/java/com/csc190/bookbazaar/Profile.java
+++ app/src/main/java/com/csc190/bookbazaar/Profile.java
@@ -10,44 +10,68 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class Profile extends Activity {
     FirebaseDatabase database;// = FirebaseDatabase.getInstance();
     DatabaseReference myRef;// = database.getReference("user");
     FirebaseAuth mAuth;
+    FirebaseFirestore fStore;
     FirebaseUser user;
+    DocumentReference docRef;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+
         user = FirebaseAuth.getInstance().getCurrentUser();
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference("User"); // .child(user_key) to get the user logged in
         mAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        docRef = fStore.collection("users").document(user.getUid());
+
         // 3 texts, 1 button
         final Button editProfile;
-        final TextView email;
+        final TextView email, username, hofid;
         email = findViewById(R.id.textView25);
+        username = findViewById(R.id.textView7);
+        hofid = findViewById(R.id.textView9);
+
         String em = user.getEmail();
-       // String passWord = user.getPass();
-      //  String eMail = us.getEmail();
-//
-       // user.setText(userName);
         email.setText(em);
-       // pass.setText(passWord);
+
         editProfile = findViewById(R.id.button8);
+
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()) {
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    String name = documentSnapshot.getString("username");
+                    username.setText(name);
+                    String id = documentSnapshot.getString("HofstraID");
+                    hofid.setText(id);
+                }
+            }
+        });
+
         editProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
                 startActivity(new Intent(getApplicationContext(), EditProfile.class));
             }});
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.tab_profile);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
