36e56244c90ec532fb0b72b86cf47adbc0015e92
lizzBee <ehorishny@gmail.com>
2020-11-07 14:31:10 -0500

    editing pass and username is fully functional
    Signed-off-by: lizzBee <ehorishny@gmail.com>

--- app/src/main/java/com/csc190/bookbazaar/EditProfile.java
+++ app/src/main/java/com/csc190/bookbazaar/EditProfile.java
@@ -3,6 +3,7 @@ package com.csc190.bookbazaar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,49 +14,80 @@ import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EditProfile extends Activity {
     FirebaseDatabase database;// = FirebaseDatabase.getInstance();
     DatabaseReference myRef;// = database.getReference("user");
+    FirebaseFirestore fStore;
     FirebaseAuth mAuth;
     FirebaseUser user;
+    DocumentReference docRef;
+    UserInfo profile;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(getApplicationContext(), "WOOOO!!!!", Toast.LENGTH_SHORT).show();
         setContentView(R.layout.edit_profile);
+
         user = FirebaseAuth.getInstance().getCurrentUser();
         database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("User"); // .child(user_key) to get the user logged in
+        myRef = database.getReference("User");
         mAuth = FirebaseAuth.getInstance();
+        fStore =FirebaseFirestore.getInstance();
+        final DocumentReference docRef = fStore.collection("users").document(user.getUid());
+
         final String TAG = Registration.class.getSimpleName();
         final Button editProfile;
-        EditText username = findViewById(R.id.editTextTextPassword3);
-        EditText newPassword = findViewById(R.id.editTextTextEmailAddress);
-        final String email = username.getText().toString();
-        final String password = newPassword.getText().toString();
+        final EditText username = findViewById(R.id.editTextTextEmailAddress);
+        final EditText newPassword = findViewById(R.id.editTextTextPassword3);
+        final EditText oldPassword = findViewById(R.id.editTextTextPassword);
 
         editProfile = findViewById(R.id.button6);
         editProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                user.updatePassword(password)
-                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                final String oldpass = oldPassword.getText().toString();
+                final String name = username.getText().toString();
+                final String password = newPassword.getText().toString();
+
+                if(TextUtils.isEmpty(oldpass)) {
+                    oldPassword.setError("Old password is Required!");
+                    return;
+                }
+
+                if(!TextUtils.isEmpty(password)) {
+                    if(password.length() < 8) {
+                        newPassword.setError("Password must at least 8 characters!");
+                        return;
+                    }
+                    AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(), oldpass);
+                    user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
                             if (task.isSuccessful()) {
-                                //code to edit password->https://github.com/bikashthapa01/firebase-authentication-android/blob/master/app/src/main/java/net/smallacademy/authenticatorapp/EditProfile.java
-                                Log.d(TAG, "User password updated.");//8 chars
+                                user.updatePassword(password);
+                            } else {
+                                Toast.makeText(getApplicationContext(), "other not success", Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
+                }
+
+                if(!TextUtils.isEmpty(name)) {
+                    docRef.update("username", name);
+                }
+
                 startActivity(new Intent(getApplicationContext(), Profile.class));
             }
         });
-
-
-
 }}
