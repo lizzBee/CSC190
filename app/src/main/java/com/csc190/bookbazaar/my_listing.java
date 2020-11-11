579871644aaa6b62018d8767214fee9bc3d02076
lizzBee <ehorishny@gmail.com>
2020-11-10 21:21:51 -0500

    read listing array from firestore
    Signed-off-by: lizzBee <ehorishny@gmail.com>

--- app/src/main/java/com/csc190/bookbazaar/my_listing.java
+++ app/src/main/java/com/csc190/bookbazaar/my_listing.java
@@ -2,6 +2,7 @@ package com.csc190.bookbazaar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -9,15 +10,64 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class my_listing extends AppCompatActivity {
-
+    FirebaseDatabase database;// = FirebaseDatabase.getInstance();
+    DatabaseReference myRef;// = database.getReference("user");
+    FirebaseAuth mAuth;
+    FirebaseFirestore fStore;
+    FirebaseUser user;
+    //CollectionReference docRef;
+    DocumentReference docRef;
+    private static final String TAG =my_listing.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_listing);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("User"); // .child(user_key) to get the user logged in
+        mAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        docRef = fStore.collection("users").document(user.getUid()); //.collection("Listing");
 
+        String listings; //get listing ids
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()) {
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    ArrayList<String> list = (ArrayList<String>) documentSnapshot.get("Listing");
+                    Log.d(TAG, list.toString());
+                }
+            }
+        });
+
+        /*database.collection("cities").where("capital", "==", true)
+                .get()
+                .then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                // doc.data() is never undefined for query doc snapshots
+                console.log(doc.id, " => ", doc.data());
+            });
+        })
+    .catch(function(error) {
+            console.log("Error getting documents: ", error);
+        });*/
         Button addButton;
         addButton = findViewById(R.id.button8);
         addButton.setOnClickListener(new View.OnClickListener() {
